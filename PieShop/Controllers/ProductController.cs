8d167632fa4b5d026f79bc22772d7d13d96cbc00
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-04-17 14:47:56 +0200

    Adding controller

---
+++ PieShop/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Repositories;
+
+namespace PieShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository _productsRepository;
+        private ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductsRepository productsRepository,ICategoryRepository categoryRepository)
+        {
+            _productsRepository = productsRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            return View(_productsRepository.AllProducts);
+        }
+    }
+}

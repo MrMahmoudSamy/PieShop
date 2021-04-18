cbfa3d7d81e041daddfcc72f5db9ee87f1537599
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-04-18 09:57:11 +0200

    Adding the Packages and Creating the Context

---
+++ PieShop/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PieShop.Model;
+
+namespace PieShop.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -1,22 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PieShop.Data;
 using PieShop.Repositories;
 
 namespace PieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get;  }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(option =>
+            option.UseSqlServer(Configuration.GetConnectionString("CoffeeShop")));
             services.AddScoped<IProductsRepository, MockProductRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();

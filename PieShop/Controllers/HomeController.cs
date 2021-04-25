b25ebf6dbe53b484747f402638d92c892ba2f64e
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-04-25 15:29:03 +0200

    Home Page and partial view

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Repositories;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductsRepository _productsRepository;
+
+        public HomeController(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewmodel = new HomeViewModel
+            {
+                PiesOfTheWeek = _productsRepository.ProductOfTheWeek
+            };
+            return View(homeViewmodel);
+        }
+    }
+}
---
+++ PieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PieShop.Model;
+using System.Collections.Generic;
+
+namespace PieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Product> PiesOfTheWeek { get; set; }
+    }
+}

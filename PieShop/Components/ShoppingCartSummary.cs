b380215a6eb45942429c02f75686e39d0b6bf423
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-04-27 16:20:35 +0200

    ViewComponats

---
+++ PieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Repositories;
+using PieShop.ViewModels;
+using System.Threading.Tasks;
+
+namespace PieShop.Components
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var items =  _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}

039c3d5af69088d0a7930ef3b844bb1e8a06c2d6
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-05-11 15:42:44 +0200

    Preparing the Site for ASP.NET Core Identity

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -43,7 +43,7 @@ namespace PieShop
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
